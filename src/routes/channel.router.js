e3142d112e6cea47376c52c88a371fcdaf17d457
matu.dev <mati.a.gimenez2002@gmail.com>
2026-04-08 17:05:03 -0300

    Manejo de roles

--- src/routes/channel.router.js
+++ src/routes/channel.router.js
@@ -3,14 +3,35 @@ import channelController from '../controllers/channel.controller.js'
 import verifyWorkspaceMiddleware from '../middlewares/verifyWorkspace.middleware.js'
 import verifyMemberWorkspaceRoleMiddleware from '../middlewares/verifyMemberWorkspaceMiddleware'
 import verifyChannelMiddleware from '../middlewares/verifyChannel.middleware.js'
+import AVIABLE_MEMBER_ROLES from '../constants/roles.constant.js'
 const channelRouter = express.Router({mergeParams: true})
 
 channelRouter.use(verifyWorkspaceMiddleware)
-channelRouter.use(verifyMemberWorkspaceRoleMiddleware())
 
-channelRouter.post('/', channelController.create)
-channelRouter.get('/', channelController.getAll)
-channelRouter.delete('/:channel_id', verifyChannelMiddleware, channelController.delete)
+
+channelRouter.post(
+    '/', 
+    verifyMemberWorkspaceRoleMiddleware(
+        [AVIABLE_MEMBER_ROLES.OWNER],
+        [AVIABLE_MEMBER_ROLES.ADMIN]
+    ),
+    channelController.create
+)
+
+channelRouter.get(
+    '/', 
+    verifyMemberWorkspaceRoleMiddleware(),
+    channelController.getAll
+)
+
+channelRouter.delete(
+    '/:channel_id', 
+     verifyMemberWorkspaceRoleMiddleware(
+        [AVIABLE_MEMBER_ROLES.OWNER]
+    ),
+    verifyChannelMiddleware, 
+    channelController.delete
+)
 
 
 export default channelRouter
